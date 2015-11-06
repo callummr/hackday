d9e2197195499b70e959a670f7f5f64aa8f61888
Callum Mellor-Reed <cmellor-reed@macmillan.org.uk>
2015-11-06 11:00:04 +0000

    Add editable face form

--- app/scripts/directives/faceform.js
+++ app/scripts/directives/faceform.js
@@ -9,13 +9,31 @@
 angular.module('ngSwApp')
   .directive('faceForm', [function () {
     return {
-      template: '<div ng-repeat="(prop, val) in model"><p>{{prop}}: {{val}}</p></div>',
+      template: '' +
+        '<form>' +
+         '<div ng-repeat="(prop, val) in model" class="form-group">' +
+           '<label>{{prop}}</label>' +
+           '<!-- text -->' +
+           '<input type="text" class="form-control" ng-if="prop===\'age\'" ng-model="model[prop]">' +
+           '<!-- end text -->' +
+           '<!-- dropdown -->' +
+           '<select class="form-control" ng-if="prop!==\'age\'" ng-model="model[prop]">' +
+            '<option ng-repeat="optionVal in availableOptions[prop]">{{optionVal}}</option>' +
+           '</select>' +
+           '<!-- end dropdown -->' +
+         '</div>' +
+        '</form>' +
+        '{{model}}',
       scope: {
         model: '='
       },
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-
+        scope.availableOptions = function() {
+          return {
+            gender: ['female', 'male']
+          }
+        }();
       }
     };
   }]);
