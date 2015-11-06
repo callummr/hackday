2298bb52d1c7f13050872bda08e095bb7850ac68
Callum Mellor-Reed <cmellor-reed@macmillan.org.uk>
2015-11-06 12:12:05 +0000

    Processing page just get & show SWAPI data

--- app/scripts/controllers/processing.js
+++ app/scripts/controllers/processing.js
@@ -8,10 +8,16 @@
  * Controller of the ngSwApp
  */
 angular.module('ngSwApp')
-  .controller('ProcessingCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('ProcessingCtrl', ['$location', 'swapi', 'userFace', function ($location, swapi, userFace) {
+    var self = this;
+
+    self.userFace = userFace.getFaceData();
+    self.processing = true;
+
+    swapi.people.all().then(function(people) {
+      self.swapiData = people;
+      console.log(people);
+
+      // patryk's magic here
+    });
+  }]);
